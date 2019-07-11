488b8ffb395cb3f9eba4219812a86c33e9cc53f4
yshogo <superman199323@gmail.com>
2019-07-11 20:12:24 +0900

    show roomList

--- src/views/RoomList.vue
+++ src/views/RoomList.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="parent">
-    <div class="content">
-      <h1>部屋一覧</h1>
+  <div class="content">
+    <h1>部屋一覧</h1>
+    <div v-for="room in rooms" :key="room.key">
+      <p>{{room.id.ownerName}}</p>
+      <p>{{room.id.roomName}}</p>
     </div>
   </div>
 </template>
 
 <script>
-// import Home from "../components/Home.vue";
-
-// export default {
-//   components: {
-//     Home
-//   }
-// };
-//
-//
+import firebase from "firebase";
+export default {
+  data() {
+    return {
+      rooms: []
+    };
+  },
+  mounted() {
+    firebase
+      .firestore()
+      .collection("room")
+      .get()
+      .then(snapshot => {
+        const array = [];
+        snapshot.forEach(doc => {
+          const id = doc.id;
+          array.push({ id: doc.data() });
+        });
+        this.rooms = array;
+      });
+  }
+};
 </script>
