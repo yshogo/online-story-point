8ab0b08d2db4d944cb73f898e5b45c7c3ca4de41
yshogo <superman199323@gmail.com>
2019-07-11 21:37:27 +0900

    add input name

--- src/views/InputName.vue
+++ src/views/InputName.vue
@@ -2,6 +2,23 @@
   <div class="parent">
     <div class="content">
       <h1>名前を入力する</h1>
+      <h3 class="form-title">表示名</h3>
+      <v-text-field label="例: shogo yamada" solo />
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-checkbox></v-checkbox>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>オーナーの方はここをチェック</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
     </div>
   </div>
 </template>
+
+<style scoped>
+.form-title {
+  margin-top: 20px;
+}
+</style>
