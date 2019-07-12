ed9e79b940eeea80cab91193a153c644672e7500
yshogo <superman199323@gmail.com>
2019-07-12 11:42:02 +0900

    chenage title

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="parent">
     <div class="content">
-      <h1>オンラインストーリーポイント</h1>
+      <h1>オンラインプランニングポーカー</h1>
       <v-btn outline @click="$router.push('create_room')" color="indigo">部屋を作成する</v-btn>
       <v-btn outline @click="$router.push('room_list')" color="green">部屋を探す</v-btn>
     </div>
