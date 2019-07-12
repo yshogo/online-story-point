70a4fb39ad0b568473834fd52dee1f58cd0af1ad
yshogo <superman199323@gmail.com>
2019-07-12 10:41:17 +0900

    error handling

--- src/views/InputName.vue
+++ src/views/InputName.vue
@@ -2,7 +2,7 @@
   <div class="parent">
     <div class="content">
       <h1>名前を入力する</h1>
-      <p>ルームID ：{{ $route.params.id }}</p>
+      <p id="room_id">{{ $route.params.id }}</p>
       <h3 class="form-title">表示名</h3>
       <v-text-field v-model="name" label="例: shogo yamada" solo />
       <v-list-tile>
@@ -14,22 +14,41 @@
           <v-list-tile-title>オーナーの方はここをチェック</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
-      <v-btn
-        outline
-        color="blue"
-        @click="$router.push({name: 'story_point', params: {name: name, isChecked: isChecked, roomId: $route.params.id, id: $route.params.id}})"
-      >部屋にはいる</v-btn>
+      <v-btn outline color="blue" @click="enterRoom">部屋にはいる</v-btn>
     </div>
   </div>
 </template>
 
 <script>
+import Router from "../router";
+
 export default {
   data() {
     return {
       name: "",
       isChecked: false
     };
+  },
+  methods: {
+    enterRoom: function(event) {
+      var id = document.querySelector("#room_id").textContent;
+
+      if (id != "") {
+        Router.push({
+          name: "story_point",
+          params: {
+            name: this.name,
+            isChecked: this.isChecked,
+            roomId: id,
+            id: id
+          }
+        });
+      } else {
+        Router.push({
+          name: "room_list"
+        });
+      }
+    }
   }
 };
 </script>
